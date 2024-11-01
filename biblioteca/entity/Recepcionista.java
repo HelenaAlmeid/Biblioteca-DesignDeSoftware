bc3eeed8692322b00110c32a49e6d37ce04ab19c
Maria Fernanda <moreira.mafe@gmail.com>
2024-11-01 11:30:56 -0300

    .

--- biblioteca/entity/Recepcionista.java
+++ biblioteca/entity/Recepcionista.java
@@ -1,14 +1,21 @@
 package entity;
 import java.util.Collection;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Recepcionista {
 
 	private String nome;
-
+	@Id
 	private String cpf;
 
 	private String email;
 
+	@OneToMany (mappedBy = "emprestimo", fetch = FetchType.LAZY)
 	private Collection<Emprestimo> emprestimo;
 
 }
